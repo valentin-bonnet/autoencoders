b52a1a3f36378ddf83a36e7dcc8b555daddfde94
valentin-bonnet <valentin.qbonnet@gmail.com>
2020-03-17 14:49:08 +0900

    New method

--- train/training.py
+++ train/training.py
@@ -136,9 +136,8 @@ class Training():
             v_loss_mean.reset_states()
             v_acc_mean.reset_states()
 
-            if epoch % self.save_steps == 0 or epoch == self.epoch_max:
-                self.save()
             self.ckpt.epoch.assign_add(1)
+        self.save()
 
     def forward_epoch(self):
         print("forward epoch")
