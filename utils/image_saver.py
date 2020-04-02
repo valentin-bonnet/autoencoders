756be085ee338b1959d4517a9618ef3c6213ee13
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-02 18:40:06 +0900

    Bug fix

--- utils/image_saver.py
+++ utils/image_saver.py
@@ -63,10 +63,10 @@ def generate_and_save_images_compare_seq(model, test_input, file_name_head='imag
     x_logits = model.reconstruct(test_input)
     test_input = np.squeeze(test_input)
     x_logits = np.squeeze(x_logits)
-    print("\n\n#######\n input:\n", test_input[0, 5, :, :])
-    print("max: ", tf.reduce_max(test_input[0, 5, :, :]))
-    print("\n\n#######\n output:\n", x_logits[0, 5, :, :])
-    print("max: ", tf.reduce_max(x_logits[0, 5, :, :]))
+    #print("\n\n#######\n input:\n", test_input[0, 5, :, :])
+    #print("max: ", tf.reduce_max(test_input[0, 5, :, :]))
+    #print("\n\n#######\n output:\n", x_logits[0, 5, :, :])
+    #print("max: ", tf.reduce_max(x_logits[0, 5, :, :]))
 
     test_input_0 = test_input[:2, 0, :, :]
     test_input_5 = test_input[:2, 5, :, :]
