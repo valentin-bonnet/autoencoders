1e7a0c6eea8bab5abad27d0ccce07a280574bce7
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-06 17:21:02 +0900

    Bug fix

--- models/KVAE.py
+++ models/KVAE.py
@@ -268,9 +268,9 @@ class KVAE(tf.keras.Model):
         #tf.print(std_a.shape)
         mvn_a = tfp.distributions.MultivariateNormalTriL(mu_a, std_a)
         a_seq = mvn_a.sample()
-        print("mu_a seq shape: ", mu_a.shape)
-        print("std_a seq shape: ", std_a.shape)
-        print("a seq shape: ", a_seq.shape)
+        #print("mu_a seq shape: ", mu_a.shape)
+        #print("std_a seq shape: ", std_a.shape)
+        #print("a seq shape: ", a_seq.shape)
         log_qa_x = mvn_a.log_prob(a_seq)
         log_qa_x = tf.reduce_sum(tf.reshape(log_qa_x, [self.batch_size, self.seq_size]), [1])
 
@@ -286,9 +286,9 @@ class KVAE(tf.keras.Model):
         #log_px_a = -tf.reduce_sum(cross_ent, axis=[1, 2, 3, ])
         log_px_a = self.log_bernoulli(im, im_logit, eps=1e-6)
 
-        print("elbo : ", tf.reduce_sum(elbo_kf))
-        print("log_px|a : ", tf.reduce_sum(log_px_a))
-        print("log qa|x : ", tf.reduce_sum(log_qa_x))
+        #print("elbo : ", tf.reduce_sum(elbo_kf))
+        #print("log_px|a : ", tf.reduce_sum(log_px_a))
+        #print("log qa|x : ", tf.reduce_sum(log_qa_x))
 
         #tf.print((elbo_kf + log_px_z - log_qa_x).shape)
         loss = -tf.reduce_sum(elbo_kf + log_px_a - log_qa_x)
