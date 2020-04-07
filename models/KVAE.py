e6ecf0538f97677c3042f65c2589f19069eef7b8
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-07 21:02:24 +0900

    Bug fix

--- models/KVAE.py
+++ models/KVAE.py
@@ -424,7 +424,7 @@ class KVAE(tf.keras.Model):
         #a_arr.mark_used()
         A.mark_used()
 
-        a_arr, _ = tf.split(tf.transpose(a_arr.stack(), [1, 0, 2]), num_or_size_splits=[self.seq_size - 1, 1], axis=1)
+        a_arr, _ = tf.transpose(a_arr.stack(), [1, 0, 2])
 
         #z = tf.concat([tf.transpose(z_smooth.stack(), [1, 0, 2]), tf.expand_dims(last_z, 1)], 1)
         #std = tf.concat([tf.transpose(std_smooth.stack(), [1, 0, 2, 3]), tf.expand_dims(last_std, 1)], 1)
