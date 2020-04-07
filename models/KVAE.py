6eec24c23dc85986e0713ecb57e8ebe39366e533
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-07 16:40:52 +0900

    Bug fix

--- models/KVAE.py
+++ models/KVAE.py
@@ -221,11 +221,8 @@ class KVAE(tf.keras.Model):
             print("elbo : eigen < 0")
             print(tf.reduce_min(tf.linalg.eigvalsh(std_smooth_arr)))
             s, u, v = tf.linalg.svd(std_smooth_arr)
-            print(v.shape)
-            print(s.shape)
-            print(tf.linalg.diag(s).shape)
             h = v @ tf.linalg.diag(s) @ tf.transpose(v, [0, 1, 3, 2])
-            std_smooth_arr = (std_smooth_arr + tf.transpose(std_smooth_arr, [0, 1, 3, 2]) + h + tf.transpose(h, [0, 1, 3, 2]))/4.0
+            std_smooth_arr = (std_smooth_arr + tf.transpose(std_smooth_arr, [0, 1, 3, 2]) + h + tf.transpose(h, [0, 1, 3, 2]))/4.0 + (tf.eye(self.dim_z, dtype=tf.float64)*1e-6)
         if tf.reduce_any(tf.linalg.eigvalsh(std_smooth_arr) < 0):
             print("elbo : eigen < 0")
             print(tf.reduce_min(tf.linalg.eigvalsh(std_smooth_arr)))
@@ -328,7 +325,7 @@ class KVAE(tf.keras.Model):
             print(s.shape)
             print(tf.linalg.diag(s).shape)
             h = v @ tf.linalg.diag(s) @ tf.transpose(v, [0, 1, 3, 2])
-            std = (std + tf.transpose(std, [0, 1, 3, 2]) + h + tf.transpose(h, [0, 1, 3, 2]))/4.0
+            std = (std + tf.transpose(std, [0, 1, 3, 2]) + h + tf.transpose(h, [0, 1, 3, 2]))/4.0 + (tf.eye(self.dim_z, dtype=tf.float64)*1e-6)
         if tf.reduce_any(tf.linalg.eigvalsh(std) < 0):
             print("acc : eigen < 0")
             print(tf.reduce_min(tf.linalg.eigvalsh(std)))
