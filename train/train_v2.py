ddd39530b62b1e19ed901f6de4dd9e038f95cf95
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-07 15:59:20 +0900

    Bug fix

--- train/train_v2.py
+++ train/train_v2.py
@@ -292,11 +292,11 @@ def lr_fn(lr, step):
 lrs_fn = [lr_fn]
 batch_size = 32
 epochs_max = [40]
-saves_epochs = [2]
+saves_epochs = [50]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
 path_to_directory = directory_path+'KVAE_float64'
-step_is_epoch = True
+step_is_epoch = False
 multi = Multitraining.Multitraining(datasets, batch_size, models, optimizers, lrs, lrs_fn, epochs_max, saves_epochs, path_to_directory, step_is_epoch)
 print("Multitraining Done")
 multi.forward()
