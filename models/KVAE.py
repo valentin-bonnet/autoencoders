375093251e76836448466c7f22a7d690285b9f6a
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-07 16:00:39 +0900

    Bug fix

--- models/KVAE.py
+++ models/KVAE.py
@@ -8,7 +8,7 @@ tfd = tfp.distributions
 
 
 class KVAE(tf.keras.Model):
-    def __init__(self, layers=[64, 128, 512], latent_dim=1024, input_shape=64, sequence_length=20, dim_a=4, dim_z=2, dim_u=10, std=0.05, use_bn=False):
+    def __init__(self, layers=[64, 128, 512], latent_dim=1024, input_shape=64, sequence_length=20, dim_a=30, dim_z=4, dim_u=10, std=0.05, use_bn=False):
         super(KVAE, self).__init__()
         self.model_type = 'KVAE'
         self.batch_size = 32
