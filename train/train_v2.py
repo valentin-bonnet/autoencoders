877c882a1f34ab83f344bb65e9a3d2e5d24c2e37
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-09 14:06:19 +0900

    Bug fix

--- train/train_v2.py
+++ train/train_v2.py
@@ -292,7 +292,7 @@ def lr_fn(lr, step):
 lrs_fn = [lr_fn]
 batch_size = 8
 epochs_max = [40]
-saves_epochs = [20]
+saves_epochs = [5]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
 path_to_directory = directory_path+'KVAE_diag'
