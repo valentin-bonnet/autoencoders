a585045869684548a4421c00de4b89bbe4f59104
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-10 09:05:14 +0900

    Bug fix

--- train/train_v2.py
+++ train/train_v2.py
@@ -295,7 +295,7 @@ epochs_max = [40]
 saves_epochs = [20]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
-path_to_directory = directory_path+'KVAE_diag_3'
+path_to_directory = directory_path+'KVAE_diag_4'
 step_is_epoch = False
 multi = Multitraining.Multitraining(datasets, batch_size, models, optimizers, lrs, lrs_fn, epochs_max, saves_epochs, path_to_directory, step_is_epoch)
 print("Multitraining Done")
