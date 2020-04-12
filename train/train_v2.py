59c00542aac9e9a18879e8f2e8d14aa857336590
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-12 23:40:38 +0900

    KVAE training

--- train/train_v2.py
+++ train/train_v2.py
@@ -291,7 +291,7 @@ def lr_fn(lr, step):
 lrs_fn = [lr_fn]
 batch_size = 16
 epochs_max = [40]
-saves_epochs = [10]
+saves_epochs = [50]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
 path_to_directory = directory_path+'KVAE_fast_lr'
