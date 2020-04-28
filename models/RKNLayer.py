9939a7471148c8e261677bae889f52bf54b029b6
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-28 22:59:50 +0900

    Bug Fix

--- models/RKNLayer.py
+++ models/RKNLayer.py
@@ -7,7 +7,7 @@ class RKNLayer(tf.keras.layers.Layer):
         self.alpha_unit = alpha_unit
         self.M = m
         self.N = m*2
-        self.state_size = m*4
+        self.state_size = [m, m,m , m]
         self.output_size = m
         super(RKNLayer, self).__init__(**kwargs)
 
