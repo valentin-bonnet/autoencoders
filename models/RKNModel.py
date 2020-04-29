70d9b6e7b066ea80e4c187792241c45d5458eb97
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-29 10:03:07 +0900

    Bug Fix

--- models/RKNModel.py
+++ models/RKNModel.py
@@ -7,7 +7,6 @@ class RKNModel(tf.keras.Model):
         ## ENCODER
 
         self.inference_net = tf.keras.Sequential()
-        self.inference_net.add(tf.keras.layers.Input(shape=(input_shape, input_shape, c)))
         self.inference_net.add(tf.keras.layers.Conv2D(filters=layer_channel_1, kernel_size=3, strides=2, padding='same'))
         self.inference_net.add(tf.keras.layers.ReLU())
         self.inference_net.add(tf.keras.layers.Conv2D(filters=layer_channel_1, kernel_size=3, strides=1, padding='same'))
@@ -32,7 +31,6 @@ class RKNModel(tf.keras.Model):
         dense_output = size_decoded_frame**2 * size_decoded_layers
 
         self.generative_net = tf.keras.Sequential()
-        self.generative_net.add(tf.keras.layers.InputLayer(input_shape=(n)))
         self.generative_net.add(tf.keras.layers.Dense(dense_output))
         self.generative_net.add(tf.keras.layers.Reshape(target_shape=(size_decoded_frame, size_decoded_frame, size_decoded_layers)))
         self.generative_net.add(tf.keras.layers.Conv2DTranspose(filters=layer_channel_2, kernel_size=3, strides=1, padding='same'))
