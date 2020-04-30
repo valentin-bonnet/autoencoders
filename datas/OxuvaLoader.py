ceb5e4b7061f68a35487564761cda0158bdda606
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-01 01:59:13 +0900

    Bug fix

--- datas/OxuvaLoader.py
+++ datas/OxuvaLoader.py
@@ -15,10 +15,10 @@ def _preprocess_one_ds(parsed_data):
     img_lab = tf.py_function(func=_rgb2lab, inp=[img], Tout=tf.float32)
 
     img_lab = tf.cast(img_lab, tf.float32)
-    #img_normalized = img_lab - [0., 128.0, 128.0]
-    #img_normalized = (img_lab / [50.0, 127.5, 127.5]) - 1.0
-    #img_normalized = tf.reshape(img_normalized, [256, 256, 3])
-    return img_lab
+    img_normalized = img_lab + [0., 128.0, 128.0]
+    img_normalized = (img_normalized / [50.0, 127.5, 127.5]) - 1.0
+    img_normalized = tf.reshape(img_normalized, [256, 256, 3])
+    return img_normalized
 
 def oxuva_loader(path='/content/drive/My Drive/Colab Data/Datasets/oxuva_256/', seq_size=8):
     datasets = oxuvaTFRecord.tfrecord_to_dataset(path)
