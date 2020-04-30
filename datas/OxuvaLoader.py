ecaf08c6c0dca595ad2c161e70403e3f4cb8993c
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-04-30 17:56:36 +0900

    Bug fix

--- datas/OxuvaLoader.py
+++ datas/OxuvaLoader.py
@@ -12,9 +12,9 @@ def _rgb2lab(image):
 def _preprocess_one_ds(parsed_data):
     img = parsed_data['image_raw']
     img = tf.image.decode_jpeg(img)
-    img = tf.image.convert_image_dtype(img, tf.float32)
     img_lab = tf.py_function(func=_rgb2lab, inp=[img], Tout=tf.float32)
-    img_normalized = tf.cast(img_lab, tf.float32) + [0., 128.0, 128.0]
+    img_lab = tf.image.convert_image_dtype(img_lab, tf.float32)
+    img_normalized = img_lab + [0., 128.0, 128.0]
     img_normalized = (img_normalized / [50.0, 127.5, 127.5]) - 1.0
     return img_normalized
 
