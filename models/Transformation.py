5bd39cb47132e6c43f141f5d4f125a23e3544be1
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-01 15:33:42 +0900

    Bug fix

--- models/Transformation.py
+++ models/Transformation.py
@@ -22,11 +22,8 @@ class Transformation(tf.keras.layers.Layer):
 
             drop_ch_num = int(np.random.choice(np.arange(1, 2 + 1), 1))
             drop_ch_ind = np.random.choice(np.arange(3), drop_ch_num, replace=False)
-
-            for dropout_ch in drop_ch_ind:
-                inputs[:, :, :, :, dropout_ch] = 0
-            #inputs *= (3 / (3 - drop_ch_num))
-            return inputs
+            drop_out = 1 - tf.reduce_sum(tf.one_hot(drop_ch_ind, 3), -2)
+            return inputs * drop_out
         else:
             return inputs
 
