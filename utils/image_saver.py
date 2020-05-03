e5dde93556d29490542854bba33c734f25dbedf5
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-04 00:17:47 +0900

    Bug fix

--- utils/image_saver.py
+++ utils/image_saver.py
@@ -188,11 +188,8 @@ def KAST_View(kast, input_data, file_name_head='image', path='./'):
 
     # Gif with input / attention
     attention_unstack = tf.unstack(attention, axis=-1)
-    print("attention_unstack.shape: ", attention_unstack.shape)
     attention_concat = tf.tile(tf.expand_dims(tf.concat(attention_unstack, axis=2), -1), [1, 1, 1, 3])
-    print("attention_concat.shape: ", attention_concat.shape)
     images_attention = tf.concat([ground_truth_attention, attention_concat], axis=2).numpy()
-    print("images_attention.shape: ", images_attention.shape)
     im = []
     for image_attention in images_attention:
         im.append(Image.fromarray(np.uint8(image_attention * [255.0, 255.0, 255.0])))
