3502a1e2b566dce06c9ee76f58a69ef9d3d503c7
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-04 00:40:11 +0900

    Bug fix

--- train/train_v2.py
+++ train/train_v2.py
@@ -291,7 +291,7 @@ def lr_fn(lr, step):
 lrs_fn = [lr_fn]
 batch_size = 4
 epochs_max = [10]
-saves_epochs = [10]
+saves_epochs = [20]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
 path_to_directory = directory_path+'KAST_64_jitter'
