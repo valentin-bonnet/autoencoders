74670bb2c4c88545e38ece8c7b63c38c62faa087
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-15 01:07:53 +0900

    KAST_RKN

--- train/train_v2.py
+++ train/train_v2.py
@@ -291,7 +291,7 @@ def lr_fn(lr, step):
 lrs_fn = [lr_fn]
 batch_size = 4
 epochs_max = [5]
-saves_epochs = [100]
+saves_epochs = [10]
 #directory_path = './content/drive/My Drive/Colab Data/AE/'
 directory_path = '/content/drive/My Drive/Colab Data/AE/'
 path_to_directory = directory_path+'KAST_Resnet_RKN'
