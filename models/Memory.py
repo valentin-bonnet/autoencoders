4a475fdac4b25586f637393ef0491f0a6bafa469
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-19 12:15:31 +0900

    Bug Fix KAST_Full

--- models/Memory.py
+++ models/Memory.py
@@ -10,7 +10,7 @@ class Memory(tf.keras.layers.Layer):
         #self.lstm = tf.keras.Sequential()
         #self.lstm.add(tf.keras.layers.Input(shape=(top_a+unit, k),batch_size=4))
         #self.lstm.add(tf.keras.layers.LSTM(self.m+self.top_a, stateful=True))
-        self.state_size = ([self.m, self.k_shape], [self.m, self.v_shape], [self.m])
+        self.state_size = [[self.m, self.k_shape], [self.m, self.v_shape], [self.m]]
         self.output_size = [[self.m, self.k_shape], [self.m, self.v_shape], [self.m]]
 
         super(Memory, self).__init__(**kwargs)
