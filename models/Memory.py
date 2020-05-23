8833c50e547ab74dda7cf9fa18992a8920a3952a
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-23 22:22:30 +0900

    KAST_Patch_OneStride

--- models/Memory.py
+++ models/Memory.py
@@ -9,7 +9,7 @@ class Memory(tf.keras.layers.Layer):
         self.kernel = kernel
         self.threshold = threshold*((kernel*kernel)/100.0)
         self.hw_shape = 64*64
-        self.batch_shape = 4
+        self.batch_shape = 8
         #self.lstm = tf.keras.Sequential()
         #self.lstm.add(tf.keras.layers.Input(shape=(top_a+unit, k),batch_size=4))
         #self.lstm.add(tf.keras.layers.LSTM(self.m+self.top_a, stateful=True))
