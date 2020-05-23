fb44a24143593fed6086c466baa8fa631dd28f17
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-23 22:08:04 +0900

    KAST_Patch_OneStride

--- models/KAST.py
+++ models/KAST.py
@@ -208,6 +208,8 @@ class KAST(tf.keras.Model):
         out_arr = []
         k_next = tf.unstack(tf.expand_dims(k_next, -2), num=4096, axis=1)
         max_patch = tf.unstack(max_patch, num=4096, axis=1)
+        m_k = tf.transpose(m_k, [0, 1, 3, 2])
+        m_v = tf.transpose(m_v, [0, 1, 3, 2])
         for i in range(4096):
             m_k_one_patch = tf.gather(m_k, max_patch[i], batch_dims=1, axis=1)
             m_v_one_patch = tf.gather(m_v, max_patch[i], batch_dims=1, axis=1)
