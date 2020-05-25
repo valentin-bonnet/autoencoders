370ccf91bfdd40c4edad0ea0f6bc25856b5b5d26
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-25 23:09:59 +0900

    Memory size 500

--- train/training.py
+++ train/training.py
@@ -17,7 +17,7 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 class Training():
     def __init__(self, dataset, batch_size, model, optimizer, lr, lr_fn, epoch_max, path_to_directory, save_steps, step_is_epoch, is_seq=False):
-        self.redone = True
+        self.redone = False
         self.batch_size = batch_size
         if self.redone:
             self.train_ds = dataset.train_ds.shuffle(buffer_size=1000).batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
