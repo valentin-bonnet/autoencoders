17ccf2b4888f94c02c3c5f3e509f062a9d86b423
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-25 17:06:28 +0900

    SBAE

--- train/training.py
+++ train/training.py
@@ -19,9 +19,9 @@ class Training():
     def __init__(self, dataset, batch_size, model, optimizer, lr, lr_fn, epoch_max, path_to_directory, save_steps, step_is_epoch, is_seq=True):
         self.batch_size = batch_size
         #self.train_ds = dataset.train_ds.shuffle(buffer_size=100).batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
-        self.train_ds = dataset.train_ds.batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
+        self.train_ds = dataset.train_ds.shuffle(buffer_size=1000).batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
         self.train_size = dataset.train_size//batch_size
-        self.val_ds = dataset.val_ds.batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
+        self.val_ds = dataset.val_ds.shuffle(buffer_size=1000).batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
         self.val_size = dataset.val_size//batch_size
         #self.test_ds = dataset.test_ds.batch(batch_size, drop_remainder=True).prefetch(buffer_size=AUTOTUNE)
         self.model = model
