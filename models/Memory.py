c5bc17b256a98761d55e9ae78f5e88861f378dbd
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-05-31 15:51:10 +0900

    KAST Memory Patch

--- models/Memory.py
+++ models/Memory.py
@@ -71,9 +71,8 @@ class Memory(tf.keras.layers.Layer):
         m_k_sorted = tf.gather(self.m_k, idx, batch_dims=1, axis=1)
         m_v_sorted = tf.gather(self.m_v, idx, batch_dims=1, axis=1)
 
-        s = k @ tf.transpose(k, [0, 2, 1])  # (bs, hw, 256) @ (bs, 256, hw) = (bs, hw, hw)
+        s = tf.reduce_sum(k @ tf.transpose(k, [0, 2, 1]), -1)  # (bs, hw, 256) @ (bs, 256, hw) = (bs, hw, hw)
         print("s.shape: ", s.shape)
-        s = tf.nn.softmax(s, -1)
         print("s_softmax.shape: ", s.shape)
         max_s_m, _ = tf.math.top_k(s, k=self.m)
         print("max_s_m.shape: ", max_s_m.shape)
