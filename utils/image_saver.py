cec6652823f86983f69bb266e701f2aaafeb27b8
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-06-04 22:40:29 +0900

    KAST Memory Patch

--- utils/image_saver.py
+++ utils/image_saver.py
@@ -134,17 +134,18 @@ def KAST_test(kast, davis, file_name_head='image', path='./'):
     output_v = output_v[0].numpy()
     v_j = v_j[0].numpy()
     seq_size = output_v.shape[0]
-    print("############## \n OUTPUT \n ##############")
-    print(output_v[0])
-    print("\n\n ############## \n GROUND TRUTH \n ##############")
-    print(v_j[0])
-    print("\n\n")
 
     #LAB to RGB
     for i in range(seq_size):
         output_v[i] = cv2.cvtColor(np.float32((output_v[i] + 1.0) * [50.0, 127.5, 127.5] - [0., 128., 128.]), cv2.COLOR_Lab2RGB)
         v_j[i] = cv2.cvtColor(np.float32((v_j[i] + 1.0) * [50.0, 127.5, 127.5] - [0., 128., 128.]), cv2.COLOR_Lab2RGB)
         raw[i] = cv2.cvtColor(np.float32((raw[i] + 1.0) * [50.0, 127.5, 127.5] - [0., 128., 128.]), cv2.COLOR_Lab2RGB)
+
+    print("############## \n OUTPUT \n ##############")
+    print(output_v[0])
+    print("\n\n ############## \n GROUND TRUTH \n ##############")
+    print(v_j[0])
+    print("\n\n")
 
     #RGB to RGB
     #output_v = np.uint8((output_v+1.)*127.5)
