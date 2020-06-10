5545e1b26894cbb83327e1cb6ea1cd477195450c
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-06-11 02:38:32 +0900

    KAST Memory Patch Results

--- models/KAST.py
+++ models/KAST.py
@@ -27,8 +27,10 @@ class KAST(tf.keras.Model):
         # inputs: [(bs, T, H, W, 3), (bs, T, h, w, 3)]
         bs = inputs[1].shape[0]
         self.memory.batch_shape = bs
-        self.transformation.batch_shape = bs
+
         seq_size = inputs[1].shape[1]
+        self.transformation.batch_shape = bs
+        self.transformation.seq_size = seq_size
         H = inputs[0].shape[2]
         W = inputs[0].shape[3]
         C = inputs[0].shape[4]
