9369cd1833589845c69e05f2b34da761aefc04e6
Kinaxo <valentin.bonnet@insa-lyon.fr>
2020-06-27 16:34:34 +0900

    KAST_REDO

--- train/train_v2.py
+++ train/train_v2.py
@@ -292,7 +292,7 @@ if KAST:
     lrs_fn = [lr_fn]
     batch_size = 4
     epochs_max = [40]
-    saves_epochs = [50]
+    saves_epochs = [250]
     #directory_path = './content/drive/My Drive/Colab Data/AE/'
     directory_path = '/content/drive/My Drive/Colab Data/AE/'
     path_to_directory = directory_path+'KAST_Retrain_with_memory'
